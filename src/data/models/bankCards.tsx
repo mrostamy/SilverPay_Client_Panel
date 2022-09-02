aaa0e60f366691a2ff874789f0f4143923c4da62
mrostamy <mrostamy92@gmail.com>
2022-09-02 19:45:11 +0430

    end section 5

---
+++ src/data/models/bankCards.tsx
@@ -0,0 +1,13 @@
+export default interface BankCard {
+
+    id: string,
+    bankName: string,
+    ownerName: string,
+    accountNumber: string,
+    shaba: string,
+    cardNumber: string,
+    approve: boolean,
+    expireMonth: string,
+    expireYear: string,
+
+}
